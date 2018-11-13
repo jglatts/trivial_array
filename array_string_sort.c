8599e5ba4f619a0d81cbd2e537b58e66d80456d5
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-11-13 04:22:40 +0000

    Update array_string_sort.c

--- array_string_sort.c
+++ array_string_sort.c
@@ -1,154 +1,154 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+	#include <string.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <time.h>
 
 
-/* Sort values in the array using bubble sort */
-void sort_ray(int ray[], int size)
-{
-	int temp;
-
-	for (int i = 0; i < size - 1; ++i)
+	/* Sort values in the array using bubble sort */
+	void sort_ray(int ray[], int size)
 	{
-		for (int x = 0; x < size - i - 1; ++x)
+		int temp;
+
+		for (int i = 0; i < size - 1; ++i)
 		{
-			if (ray[x] > ray[x + 1])
+			for (int x = 0; x < size - i - 1; ++x)
 			{
-				temp = ray[x];
-				ray[x] = ray[x + 1];
-				ray[x + 1] = temp;
+				if (ray[x] > ray[x + 1])
+				{
+					temp = ray[x];
+					ray[x] = ray[x + 1];
+					ray[x + 1] = temp;
+				}
 			}
 		}
 	}
-}
 
 
-/* Generate random numbers, the even/odd comparison is to have a variety of digits */
-void load_ray(int ray[], int size)
-{
-	for (int i = 0; i < size; ++i)
+	/* Generate random numbers, the even/odd comparison is to have a variety of digits */
+	void load_ray(int ray[], int size)
 	{
-		if (i % 2 == 0)
+		for (int i = 0; i < size; ++i)
 		{
-			ray[i] = rand() % 100;
-		}
-		else
-		{
-			ray[i] = rand() % 10;
-		}
-	}
-}
-
-
-/* Change a certain value of the array */
-void examine_value(int ray[], int size)
-{
-	int idx, new_value;
-
-	printf("Enter an index To examine...\n --> ");
-	scanf("%d", &idx);
-	printf("Index[%d] --> %d\n", idx, ray[idx]);
-	printf("Enter a new value\n --> ");
-	scanf("%d", &new_value);
-	ray[idx] = new_value;
-	printf("Index[%d] --> %d\n", idx, ray[idx]);
-
-}
-
-
-/* Logical error here, tweakage to be done */
-int find_matches(int ray[], int size)
-{
-	int match_count = 0;
-
-	for (int i = 0; i < size - 1; ++i)
-	{
-		if (ray[i] == ray[i + 1])
-		{
-			printf("\n[%d] matches [%d]\n", ray[i], ray[i + 1]);
-			match_count++;
+			if (i % 2 == 0)
+			{
+				ray[i] = rand() % 100;
+			}
+			else
+			{
+				ray[i] = rand() % 10;
+			}
 		}
 	}
 
-	return match_count;
-}
 
-
-/* WIP--> working on indenting the text in 3 columns */
-void print_ray(int ray[], int size)
-{
-	int format = size / 3;	// 1st column
-	int mid_format = format * 2; // 2nd column
-	int last_format = format * 3; // 3rd column
-
-	printf("%d\n", format);
-	printf("%d\n", mid_format);
-	printf("%d\n", last_format);
-
-	for (int i = 0; i <= size; ++i)
+	/* Change a certain value of the array */
+	void examine_value(int ray[], int size)
 	{
-		printf("\t[%d]->%d\t\t", i, ray[i]);
-		/*
-		if ( i >= mid_format)
+		int idx, new_value;
+
+		printf("Enter an index To examine...\n --> ");
+		scanf("%d", &idx);
+		printf("Index[%d] --> %d\n", idx, ray[idx]);
+		printf("Enter a new value\n --> ");
+		scanf("%d", &new_value);
+		ray[idx] = new_value;
+		printf("Index[%d] --> %d\n", idx, ray[idx]);
+
+	}
+
+
+	/* Logical error here, tweakage to be done */
+	int find_matches(int ray[], int size)
+	{
+		int match_count = 0;
+
+		for (int i = 0; i < size - 1; ++i)
 		{
-			printf("\t\t[%d] --> %d\t\t", i, ray[i]);
+			if (ray[i] == ray[i + 1])
+			{
+				printf("\n[%d] matches [%d]\n", ray[i], ray[i + 1]);
+				match_count++;
+			}
 		}
-		else if (i >= last_format)
+
+		return match_count;
+	}
+
+
+	/* WIP--> working on indenting the text in 3 columns */
+	void print_ray(int ray[], int size)
+	{
+		int format = size / 3;	// 1st column
+		int mid_format = format * 2; // 2nd column
+		int last_format = format * 3; // 3rd column
+
+		printf("%d\n", format);
+		printf("%d\n", mid_format);
+		printf("%d\n", last_format);
+
+		for (int i = 0; i <= size; ++i)
 		{
-			printf("\t\t[%d] --> %d\t\t", i, ray[i]);
+			printf("[%d]->%d\t\t", i, ray[i]);
+			/*
+			if ( i >= mid_format)
+			{
+				printf("\t\t[%d] --> %d\t\t", i, ray[i]);
+			}
+			else if (i >= last_format)
+			{
+				printf("\t\t[%d] --> %d\t\t", i, ray[i]);
+			}
+			else
+			{
+				printf("\t[%d] --> %d\t", i, ray[i]);
+			}
+			*/
 		}
-		else
+
+	}
+
+
+	int main(int argc, char const *argv[])
+	{
+		int size, matches, compare, find_compare;
+		char check[] = "SORT";
+		char find_check[] = "FIND";
+		char usr_input[5];
+		char find_input[5];
+
+		printf("%s\n", *argv);
+		printf("%d\n", argc);
+
+		srand(time(NULL));
+		printf("\nEnter Limit For Array\n--> ");
+		scanf("%d", &size);
+		int ray[size];
+
+		load_ray(ray, size);
+		for (int i = 0; i < size; ++i)
 		{
-			printf("\t[%d] --> %d\t", i, ray[i]);
+			printf("\n[%d] --> %d\n", i, ray[i]);
 		}
-		*/
+
+		matches = find_matches(ray, size);
+
+		printf("\n Found %d matches\n", matches);
+		printf("\nEnter SORT to sort through values\n");
+		printf("Enter FIND to examine a value\n --> ");
+		scanf("%s", usr_input);
+
+		compare = strcmp(usr_input, check);
+		find_compare = strcmp(usr_input, find_check);
+		if (compare == 0)
+		{
+			sort_ray(ray, size);
+			print_ray(ray, size);
+		}
+		else if (find_compare == 0)
+		{
+			examine_value(ray, size);
+			print_ray(ray, size);
+		}
+
+		return 0;
 	}
-
-}
-
-
-int main(int argc, char const *argv[])
-{
-	int size, matches, compare, find_compare;
-	char check[] = "SORT";
-	char find_check[] = "FIND";
-	char usr_input[5];
-	char find_input[5];
-
-	printf("%s\n", *argv);
-	printf("%d\n", argc);
-
-	srand(time(NULL));
-	printf("\nEnter Limit For Array\n--> ");
-	scanf("%d", &size);
-	int ray[size];
-
-	load_ray(ray, size);
-	for (int i = 0; i < size; ++i)
-	{
-		printf("\n[%d] --> %d\n", i, ray[i]);
-	}
-
-	matches = find_matches(ray, size);
-
-	printf("\n Found %d matches\n", matches);
-	printf("\nEnter SORT to sort through values\n");
-	printf("Enter FIND to examine a value\n --> ");
-	scanf("%s", usr_input);
-
-	compare = strcmp(usr_input, check);
-	find_compare = strcmp(usr_input, find_check);
-	if (compare == 0)
-	{
-		sort_ray(ray, size);
-		print_ray(ray, size);
-	}
-	else if (find_compare == 0)
-	{
-		examine_value(ray, size);
-		print_ray(ray, size);
-	}
-
-	return 0;
-}
